84ae967da559e9e388a4d60d7b7d2ca73fcade6b
Farhan Tahmid <farhantahmid881@gmail.com>
2024-05-22 10:54:56 +0600

    second page and the database done

--- mystery_solver_webapp/app/urls.py
+++ mystery_solver_webapp/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name="app"
 
 urlpatterns = [
-    path('',views.welcome_view,name="welcome_view")
+    path('',views.welcome_view,name="welcome_view"),
+    path('story/<int:story_pk>',views.stories,name="story"),
 ]
--- mystery_solver_webapp/app/views.py
+++ mystery_solver_webapp/app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Stories
 # Create your views here.
 
 def welcome_view(request):
     # returns the welcome view of the screen
     return render(request,'index.html')
 
+def stories(request,story_pk):
+    # shows the stories and calls functions to resolve the stories
+    
+    get_story=Stories.objects.get(pk=story_pk)
+    
+    print(get_story.plot_of_the_murder)
+    return render(request,'stories.html')
+    
